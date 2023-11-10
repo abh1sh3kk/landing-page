8f52fcd0377489f528eeb649c167cd0c46db5f14
abh1sh3kk <abhishekacharya171@gmail.com>
2023-11-10 17:42:51 +0545

    finally build the card and make it responsive

--- src/Components/CaseStudy.jsx
+++ src/Components/CaseStudy.jsx
@@ -1,59 +1,108 @@
+import { Button, Text, Card, Flex, Stack, Title } from "@mantine/core";
+
 import {
-  Button,
-  Text,
-  Card,
-  Flex,
-  Group,
-  Paper,
-  Stack,
-  Title,
-} from "@mantine/core";
-import { Icon12Hours, Icon2fa } from "@tabler/icons-react";
+  AiFillAccountBook,
+  AiFillGithub,
+  AiFillLinkedin,
+} from "react-icons/ai";
 
 const CaseStudy = () => {
   return (
     <section>
-      <Paper
-        gap={10}
-        withBorder
+      <Card
         component={Flex}
         direction={{ sm: "column", lg: "row" }}
+        radius={12}
+        className="case-study-card"
+        maw={800}
+        bg="transparent"
+        w="90vw"
       >
-        <Group
-          component={Stack}
-          p={32}
-          align="center"
-          style={{ border: "1px solid red" }}
+        {/* ---------------------Left---------------------------- */}
+        <Stack
+          style={{
+            borderRight: "1px solid #373737f0",
+            width: "100%",
+          }}
+          className="card-radial"
+          w={{ xs: "100%", lg: "280px !important" }}
+          p="xl"
+          justify="center"
         >
-          <Stack align="center">
-            <Icon2fa />
-            <Stack align="center">
-              <Text>Integrated Apps</Text>
-              <Flex>
-                <Icon12Hours />
-                <Icon12Hours />
-                <Icon12Hours />
+          {/* ----------------------circle-------------- */}
+          <div className="circle-gradient"></div>
+
+          <Stack
+            align="center"
+            justify="space-between"
+            gap="xl"
+            style={{ position: "relative" }}
+          >
+            <Title order={2}>Pitch</Title>
+            {/* ----------------------Integrated and logos-------------- */}
+            <Stack
+              align="center"
+              // visibleFrom="md"
+            >
+              <Text size="xl">Integrated Apps</Text>
+              <Flex gap="sm">
+                <AiFillGithub
+                  style={{
+                    backgroundColor: "white",
+                    borderRadius: "50%",
+                    padding: "0.2rem",
+                  }}
+                  fill="#6a0bffb0"
+                  fontSize={36}
+                />{" "}
+                <AiFillAccountBook
+                  style={{
+                    backgroundColor: "white",
+                    borderRadius: "50%",
+                    padding: "0.2rem",
+                  }}
+                  fill="#6a0bffb0"
+                  fontSize={36}
+                />
+                <AiFillLinkedin
+                  style={{
+                    backgroundColor: "white",
+                    borderRadius: "50%",
+                    padding: "0.2rem",
+                  }}
+                  fill="#6a0bffb0"
+                  fontSize={36}
+                />
               </Flex>
             </Stack>
-            <Button variant="white" color="black">
+
+            {/* ----------------------Case study button-------------- */}
+            <Button variant="white" radius="xl" size="md" color="black">
               Case Study
             </Button>
           </Stack>
-        </Group>
+        </Stack>
 
-        <Stack p={16} gap={16} maw={720}>
+        {/* ---------------------Right---------------------------- */}
+        <Stack
+          className="case-study-card__details"
+          w={{ sm: "100%", lg: "520px" }}
+          p="xl"
+        >
           <Stack gap={2}>
-            <Title order={4}>Jeremy Dahan</Title>
-            <Text>Design System Designer</Text>
+            <Title order={3} c="white">
+              Jeremy Dahan
+            </Title>
+            <Text size="xl">Design System Designer</Text>
           </Stack>
-          <Text>
+          <Text size="xl" c="white">
             "Specify brought us confidence and ease of mind. Before, we used to
             be cautious and hesitant before updating our design tokens. We're
             now confident about updating a token, rolling back to a previous
             version, or even trying new ones."
           </Text>
         </Stack>
-      </Paper>
+      </Card>
     </section>
   );
 };
